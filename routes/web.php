332c7a4e88ac123a090849f35dc73d4044244871
Ahmed zaqowt <ahmadzaqowt@gmail.com>
2025-04-12 18:54:12 +0300

    Route&Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteOne\SiteOneController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home' , function() {
        return 'Home , Home';
+});*/
+// user , profile
+/*
+Route::get('/user/profile/{id}' , function($id) {
+    echo 'user id : ' . $id ;
 });
+*/
+/*
+// news , all news , new
+Route::get('/news/{id?}' , function($id = null) {
+    $news = [1 , 2 , 3 , 6 , 8 ] ;
+
+    if($id){
+      echo 'new is : ' . $id ;
+    }else{
+        echo 'all news : <br>' ;
+        foreach($news as $new){
+            echo $new . '<br>' ;
+         }
+    }
+});*/
+
+
+// site 1 / home , about us , contact us
+// home
+// prefix
+
+Route::prefix('site1')->controller(SiteOneController::class)->group(function(){
+    Route::get('/home' , 'home');
+    Route::get('/about' ,  'about');
+    Route::get('/contact' ,  'contact');
+    Route::get('/msg/{id}' ,  'msg');
+});
+
+
+
+
+
+
+
+
 
 
 
